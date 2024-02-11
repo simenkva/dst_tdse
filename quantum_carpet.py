f7309743c5f9c30947209e4ff858666bbc0c5fe4
simenkva <simen.kvaal@gmail.com>
2024-02-11 20:55:52 +0100

    update

--- quantum_carpet.py
+++ quantum_carpet.py
@@ -5,7 +5,7 @@ from matplotlib.animation import FuncAnimation
 import matplotlib.pyplot as plt
 
 nx = 2047
-n_steps = 2048
+n_steps = 2048*2
 dt = np.pi/n_steps
 solver = DSTSolver(0.0, np.pi, nx)
 x = solver.xx[0]
@@ -14,7 +14,7 @@ solver.set_potential_operator(lambda x: 0.0)
 solver.prepare_for_propagation(dt)
 
 psi = np.ones(nx, dtype=complex)
-
+             
 psi_hist = np.zeros((n_steps+1, nx), dtype=complex)
 psi_hist[0] = psi
 
@@ -32,7 +32,7 @@ fig = plt.figure(figsize=(8, 8))
 def update(frame):
     plt.clf()
     plt.plot(x, np.abs(psi_hist[frame])**2, 'k')
-    plt.text(0.1, 4.5, f't = {frame*dt:.2f}', fontsize=12)
+    plt.text(0.1, 4.5, f'$t = {frame*dt:.2f}$', fontsize=12)
     plt.ylim(0, 5)
     plt.xlim(0, np.pi)
     plt.xlabel(r'$x$')
